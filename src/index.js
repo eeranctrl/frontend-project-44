df6d6be44034458be6b4c34d59c9db1389e69d0f
eeranctrl <pavlovskiybogdan890@gmail.com>
2026-03-18 01:12:31 +0300

    created game engine

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import askQuestion from './cli.js'
+
+const ROUNDS = 3
+
+export default function main(description_game, roundData) {
+  console.log('Welcome to the Brain Games!')
+  const name = askQuestion('May I have your name?')
+  console.log(`Hello, ${name}!`)
+
+  console.log(description_game)
+
+  for (let i = 0; i < ROUNDS; i += 1) {
+    const [question, correctAnswer] = roundData()
+
+    console.log(`Question: ${question}`)
+    const userAnswer = askQuestion('Your answer: ')
+
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+      console.log(`Let's try again, ${name}`)
+      return
+    }
+
+    console.log('Correct!')
+  }
+
+  console.log(`Congratulations, ${name}`)
+}
